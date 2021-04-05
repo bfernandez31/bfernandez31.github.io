bdd428401509c0a43678b6eacedf9389c9cc7ae1
b.fernandez <b.fernandez@groupeonepoint.com>
2021-04-05 23:34:23 +0200

    test animation on scroll and add section

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ import barba from '@barba/core';
 import {gsap} from "gsap";
 import './scss/main.css';
 
+
+// fullpage
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    navigationTooltips: ['Home', 'Works', 'About Me', 'Contact'],
+    scrollHorizontally: true,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom',
+    fixedElements: 'header',
+    onLeave: (origin, destination, direction) => {
+        const section = destination.item;
+        if (destination.index !== 1) {
+            const title = section.querySelector('h1');
+            console.log(title);
+            const t1 = gsap.timeline();
+            t1.fromTo(title, 1, {x: '-30%', opacity: 0}, {x: '10%', opacity: 1});
+        }
+    }
+})
+
+
 // barba js
 function pageTransition() {
     let t1Page = gsap.timeline();
